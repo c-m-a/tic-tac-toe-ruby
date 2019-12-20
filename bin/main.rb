0ac6e4dcbe1d119acb81d5b1656057c23e16ea71
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2019-12-20 16:26:27 -0500

    Add display_board method and fix some linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 POSITIONS = {
   1 => [0, 0],
@@ -12,23 +13,21 @@ POSITIONS = {
   9 => [2, 2]
 }.freeze
 
-title = ' Tic Tac Toe Game '
+TITLE = ' Tic Tac Toe Game '
 winner = false
-last_row = " ------------------------- ".center(80)
-middle_row = " |-----------------------| ".center(80)
 
 players = {
   0 => { name: nil, token: 'x' },
-  1 => { name: nil, token: 'o'}
+  1 => { name: nil, token: 'o' }
 }
 
 board = Array.new(3) { Array.new(3) }
 
-def display_header(title)
+def display_header
   puts `clear`
-  puts "".center(80, '=')
-  puts title.center(80, '=')
-  print "".center(80, '=') + "\n\n"
+  puts ''.center(80, '=')
+  puts TITLE.center(80, '=')
+  print ''.center(80, '=') + "\n\n"
 end
 
 def ask_names(players = Array)
@@ -36,67 +35,83 @@ def ask_names(players = Array)
     player_number = n + 1
     print "Type the name of the player #{player_number}: "
     name = gets.chomp
-    name = "Player " + (player_number).to_s if name.empty?
+    name = 'Player ' + player_number.to_s if name.empty?
     players[n][:name] = name.capitalize
     print "Hi #{name.capitalize}! Welcome to the Game! :)\n\n"
   end
 end
 
-def display_who_starting(current_name)
-  puts " Choosing randomdly who is going to start the game! :) ".center(80, '=')
+def display_who_starting(current_name, wait = 2)
+  puts ' Choosing randomdly who is going to start the game! :) '.center(80, '=')
   puts "\n\n"
   puts "Great #{current_name}!! You're starting first and you're (x))! :)".center(80)
 
   puts "\nWait a moment to start the game!...".center(80)
-  sleep(3)
+  sleep(wait)
   puts `clear`
 end
 
-display_header(title)
+def display_board(board = Array)
+  row = 1
+  step = 0
+  last_row = ' ------------------------- '.center(80)
+  middle_row = ' |-----------------------| '.center(80)
 
-while 1
+  puts ''.center(80, '=')
+  puts TITLE.center(80, '=')
+  print ''.center(80, '=') + "\n\n"
+  puts ' ------------------------- '.center(80)
+
+  board.each do |x, y, z|
+    x = x.nil? ? step + 1 : x
+    y = y.nil? ? step + 2 : y
+    z = z.nil? ? step + 3 : z
+    puts " |   #{x}   |   #{y}   |   #{z}   | ".center(80)
+    puts row != 3 ? middle_row : last_row
+    row += 1
+    step += 3
+  end
+end
+
+def check_winner
+  n = rand(0..1)
+  booleans = [true, false, -1]
+  booleans[n]
+end
+
+display_header
+
+loop do
   ask_names(players)
 
   current_player = rand(0..1)
   current_name = players[current_player][:name]
+
   display_who_starting(current_name)
 
   # TODO: Starting new Game
   # ttt = Game.new()
 
-  while winner == false
-    puts "".center(80, '=')
-    puts title.center(80, '=')
-    print "".center(80, '=') + "\n\n"
-    row = 1
-    step = 0
-    puts " ------------------------- ".center(80)
+  loop do
+    puts `clear`
+    display_board(board)
 
-    board.each do |x, y, z|
-      x = x.nil? ? step + 1 : x
-      y = y.nil? ? step + 2 : y
-      z = z.nil? ? step + 3 : z
-      puts " |   #{x}   |   #{y}   |   #{z}   | ".center(80)
-      puts row != 3 ? middle_row : last_row
-      row += 1
-      step += 3
-    end
+    break if winner == true
 
     loop do
-      print "\n#{current_name} type the position's number': "
+      print "\n=> #{current_name} type the position's number': "
       position = gets.chomp
       position = position.to_i
 
-      if !position.between?(1,9)
-        puts "Upss! #{current_name} wrong number! Type a number from 1-9"
+      unless position.between?(1, 9)
+        puts "Upss! => #{current_name} wrong number! Type a number from 1-9."
         next
       end
 
-      p position.class
       xy_pos = POSITIONS[position]
 
       if xy_pos.nil?
-        puts "Upss! #{current_name} wrong input :(! Type a number from 1-9"
+        puts "Upss! => #{current_name} wrong input :(! Type a number from 1-9."
         next
       end
 
@@ -108,15 +123,29 @@ while 1
       end
     end
 
-    current_player = current_player == 0 ? 1 : 0
+    current_player = current_player.zero? ? 1 : 0
     current_name = players[current_player][:name]
-    puts `clear`
-    winner = false
+    winner = check_winner
+    puts winner
   end
 
-  puts "\nTo restart press (Enter)"
-  puts "To quit press (q)!"
-  break if gets.chomp.strip.downcase == 'q'
+  next unless [true, -1].include? winner
+
+  # If it's tie
+  msg = winner == -1 ? 'Sorry Guys is a tie' : "Congratulations (#{current_name})! You win!"
+  winner = false
+  print "\n\n"
+  print msg.center(80)
+  puts "\nTo restart press (r)"
+  puts 'To quit press (q)!'
+  cmd = gets.chomp.strip.downcase
+
+  case cmd
+  when 'q'
+    break
+  when 'r'
+    board = Array.new(3) { Array.new(3) }
+  end
 end
 
 puts 'Thanks for playing, bye!! :)'
