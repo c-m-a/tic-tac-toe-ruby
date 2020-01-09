793f1d22482b4546a6b5aaea0467ff27958cd332
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-01-09 12:30:27 -0500

    Add rows and columns methods to check if there is a winner

--- lib/game.rb
+++ lib/game.rb
@@ -13,7 +13,34 @@ class Game
   end
 
   def over?
-    # Check board
+    check_winner
     @status
+  end
+
+  private
+  def check_winner
+    check_rows
+  end
+
+  def check_rows
+    t0 = @players[0][:token]
+    t1 = @players[1][:token]
+    @board.each do |x, y, z|
+      if t0 == x && t0 == y && t0 == z
+        @status = true
+        break
+      end
+
+      if t1 == x && t1 == y && t1 == z
+        @status = true
+        break
+      end
+    end
+  end
+
+  def check_columns
+  end
+
+  def check_diagonals
   end
 end
