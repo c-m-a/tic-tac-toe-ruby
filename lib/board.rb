08f943a23b1f5962bc93fe25d1a0a7693fc4c5f0
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-01-10 15:46:52 -0500

    Fix: set_position Method parameter must be at least 3 characters long

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 class Board
   attr_reader :board
 
-  def initialize()
+  def initialize
     @board = Array.new(3) { Array.new(3) }
   end
 
-  def set_position(x, y, token)
-    @board[x][y] = token
+  def set_position(x_pos, y_pos, token)
+    @board[x_pos][y_pos] = token
   end
 
-  def position_empty?(x, y)
-    @board[x][y].nil?
+  def position_empty?(x_pos, y_pos)
+    @board[x_pos][y_pos].nil?
   end
 end
