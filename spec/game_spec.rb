e75c57d93ee25ad1e5b4ba6a8c28a00c7e48739b
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-01-15 11:45:26 -0500

    Add Returns nil 3x3 Array context case to constructor

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe Game do
         expect(game.show_board).to be_kind_of Array
       end
     end
+    context 'Returns nil 3x3 Array' do
+      let(:game) { Game.new }
+      it 'returns nil 3x3 Array' do
+        expect(game.show_board).to eq Array.new(3) { Array.new(3) }
+      end
+    end
     context 'returns player\'s name number 1' do
       let(:game) { Game.new }
       it 'returns Player 1' do
